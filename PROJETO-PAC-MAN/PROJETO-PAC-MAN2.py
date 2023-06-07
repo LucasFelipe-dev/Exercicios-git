8776676c10eebf8cfa76bef21b4476bb272791fb
Lucas Tenorio <lucasfeliph015@gmail.com>
2023-06-06 23:58:30 -0300

    Publicando o projeto PAC-MAN1.py

---
+++ PROJETO-PAC-MAN/PROJETO-PAC-MAN2.py
@@ -0,0 +1,66 @@
+from random import *
+import time
+import os
+from emoji import *
+import winsound
+
+# Grid
+largura = 20
+altura = 10
+
+# Pacman
+pacmin = emojize('\U0001f612')
+comida = emojize('\U0001f354')
+pacman = emojize('\U0001f60b')
+
+# Função para gerar uma nova posição aleatória
+def posicao():
+    return [randint(0, 9), randint(0, 19)]
+
+# Função para exibir o grid
+def exibir_grid():
+    for linha in range(altura):
+        for coluna in range(largura):
+            if [linha, coluna] == posicao_pacman:
+                if exibir_pacmin:
+                    print(pacmin, end='')
+                else:
+                    print(pacman, end='')
+            elif [linha, coluna] == posicao_comida:
+                print(comida, end='')
+            else:
+                print('.', end='')
+        print()
+
+# Loop principal do jogo
+exibir_pacmin = True  # Variável auxiliar para controlar qual personagem exibir
+posicao_pacman = posicao()
+posicao_comida = posicao()
+
+while True:
+    # Limpar a tela
+    os.system('cls')
+    # Exibir o grid
+    exibir_grid()
+
+    # Verificar se o Pacman encontrou a comida
+    if posicao_pacman == posicao_comida:
+        # Gerar o beep
+        winsound.Beep(frequency= 3000, duration= 50)
+        # Gerar novas posições para o pacmin e comida
+        posicao_pacman = posicao()
+        posicao_comida = posicao()
+        exibir_pacmin = not exibir_pacmin
+
+    # Atualizar a posição do Pacman automaticamente
+    if posicao_pacman[0] < posicao_comida[0]:
+        posicao_pacman[0] += 1
+    elif posicao_pacman[0] > posicao_comida[0]:
+        posicao_pacman[0] -= 1
+    elif posicao_pacman[1] < posicao_comida[1]:
+        posicao_pacman[1] += 1
+    elif posicao_pacman[1] > posicao_comida[1]:
+        posicao_pacman[1] -= 1
+
+    # Aguardar um tempo para exibir o próximo frame (0.6 segundos neste exemplo)
+    time.sleep(0.6)
--- README.md
+++
@@ -1,5 +0,0 @@
-Olá Pessoal tudo bem, sou estudante de Analise e desenvolvimento de sistemas!
-
-Entusiasta da tecnologia e em constante aprendizado.
-
-
